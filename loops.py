948a13e53f5292366a7e74e5affcfd566602ad47
yahaira87 <109757636+yahairaa87@users.noreply.github.com>
2022-10-04 17:36:18 +0000

    loops

--- loops.py
+++ loops.py
@@ -1,43 +1,74 @@
+def loop():
+  # names = ["mary", "john", "davis", "thomas", "janette"]
 
-###################################loops intro######################################
-# queue videos
-#what is iteration?
-#what are for loops?
+  # for name in names: 
+  #   print(f"hello {name}")
 
+  # a = 1 
+  # while a < 10 : 
+  #   print(a)
+  #   a = a + 1
 
-# For Loops Practice #1
-# Using For loops, greet all members of a class, printing "Hello" + their name.
+  # num_list = [1,2,3,4,5,6,7,8,9,10]
+  # sum = 0 
+  # for num in num_list: 
+  #   sum = sum + num
+  #   print(sum)
+  # names = ["mary", "john", "davis", "thomas", "janette"]
+  # for name in names:
+  #   if name.startswith("j"):
+  #     print(f"this name starts with {name} the letter j")
 
-# For example: "Hello Norville"
+  dic = {"ke1": "a", "key2": "b", "key3" : "c"}
+  # for item in dic.items():
+  #   print(item)
+  for a,b, in dic.items():
+    print(a,b)
 
-students = ["Norville", "Fred", "Velma", "Daphne"]
-
-
-
-
-# For Loops Practice #2
-# Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
-
-list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-# sum_numbers = 
-
-
-
-
-
-# For Loops Practice #3
-# Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
-
-list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-
-# *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
-
-# num % 2 == 0 (even values)
-
-# num % 2 == 1 (odd values)
-
-list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-
-# sum_even = 
-
-# sum_odd = 
+  # while loops
+  coins = 30
+  while coins < 40:
+    print(f"I have {coins} coins")
+    coins += 1
+  ###################################loops intro######################################
+  # queue videos
+  #what is iteration?
+  #what are for loops?
+  
+  
+  # For Loops Practice #1
+  # Using For loops, greet all members of a class, printing "Hello" + their name.
+  
+  # For example: "Hello Norville"
+  
+  students = ["Norville", "Fred", "Velma", "Daphne"]
+  
+  
+  
+  
+  # For Loops Practice #2
+  # Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
+  
+  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  # sum_numbers = 
+  
+  
+  
+  
+  
+  # For Loops Practice #3
+  # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
+  
+  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  
+  # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
+  
+  # num % 2 == 0 (even values)
+  
+  # num % 2 == 1 (odd values)
+  
+  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  
+  # sum_even = 
+  
+  # sum_odd = 
--- main.py
+++ main.py
@@ -7,11 +7,18 @@
 # Random in python
 # List comprehension
 #importing modules
+# moduralize your code
 #work on creating and understanding functions
 
-from decision_making import decisions
-from comparison_operators import comparison
+# from decision_making import decisions
+# from comparison_operators import comparison
+from loops import loop
+# from enumberators import numerator
+# from list_comprehension import list
 # from dictionaries_loops import dictionaries_loops
 
-decisions()
-comparison()
+# list()
+# numerator()
+# decisions()
+# comparison()
+loop()
